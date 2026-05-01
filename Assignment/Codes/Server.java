30b3dce86be70fa43f4a33769dfa15be558b1a7c
SOHEL RANA <sohelrana104322@gmail.com>
2026-05-02 01:36:19 +0600

    Server code

--- Assignment/Codes/Server.java
+++ Assignment/Codes/Server.java
@@ -17,10 +17,77 @@ public class Server {
                 Socket s = ss.accept();
                 System.out.println("New client is connected.");
 
-                ClientHandler 
+                ClientHandler ch = new ClientHandler(s);
+                clientList.add(ch);
+                System.out.println("Total clients: " + Server.clientList.size());
+
+                new Thread(ch).start();
             }
         } catch (Exception e) {
             System.out.println("Server error: " + e);
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    Socket socket;
+    DataInputStream din;
+    DataOutputStream dout;
+    String name;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+
+        try {
+            din = new DataInputStream(socket.getInputStream());
+            dout = new DataOutputStream(socket.getOutputStream());
+
+            // first message is client name 
+            name = din.readUTF();
+            System.out.println(name + " is connected.");
+
+            broadCast(name + " joined the chatroom", this);
+        } catch (Exception e) {
+            System.out.println("ClientHandler error: " + e);
+        }
+    }
+
+    @Override
+    public void run() {
+        String msg;
+
+        try {
+            while (true) {
+                msg = din.readUTF();
+
+                if (msg.equalsIgnoreCase("exit")) {
+                    System.out.println(name + " is disconnected");
+
+                    broadCast(name + " left the chatroom", this);
+                    Server.clientList.remove(this);
+                    socket.close();
+                    break;
+                }
+            
+                System.out.println(name + ": " + msg);
+                broadCast(name + ": " + msg, this);
+            }
+        } catch (Exception e) {
+            System.out.println(name + "connection error: " + e);
+        }
+    }
+
+    // send message to all client except the sender
+    void broadCast(String message, ClientHandler sender) {
+        for (ClientHandler client : Server.clientList) {
+            try {
+                if (client != sender) {
+                    client.dout.writeUTF(message);
+                    client.dout.flush();
+                }
+            } catch (Exception e) {
+                System.out.println("Broadcast error: " + e);
+            }
         }
     }
 }
